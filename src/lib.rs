ab3c32a72a00b7c2c9d75d3bbdbbae3567820f5e
Jan Bujak <j@exia.io>
2023-04-23 18:11:15 +0000

    When iterating over lengths only iterate over those which exist in the vocab

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use pyo3::prelude::*;
 use ahash::AHashMap as HashMap;
+use ahash::AHashSet as HashSet;
 
 struct Tokenizer {
-    first_bytes_to_len: Vec<usize>,
+    first_bytes_to_lengths: Vec<Box<[u16]>>,
     bytes_to_token_index: HashMap<Vec<u8>, u16>,
     token_index_to_bytes: Vec<Vec<u8>>,
 }
@@ -14,24 +15,41 @@ lazy_static::lazy_static! {
         let mut first_bytes_to_len = Vec::new();
         first_bytes_to_len.resize(u16::MAX as usize, 2);
 
+        let mut first_bytes_to_lengths = Vec::new();
+        first_bytes_to_lengths.resize(u16::MAX as usize, {
+            let mut set = HashSet::new();
+            set.insert(1);
+            set
+        });
+
         let mut token_index_to_bytes = Vec::new();
         token_index_to_bytes.resize_with(u16::MAX as usize, Vec::new);
 
         let mut bytes_to_token_index = HashMap::new();
         for (token_bytes, token_index) in list {
             if token_bytes.len() >= 2 {
-                let max_length = &mut first_bytes_to_len[u16::from_ne_bytes([token_bytes[0], token_bytes[1]]) as usize];
+                let key = u16::from_ne_bytes([token_bytes[0], token_bytes[1]]) as usize;
+                let max_length = &mut first_bytes_to_len[key];
                 if token_bytes.len() > *max_length {
                     *max_length = token_bytes.len();
                 }
+
+                first_bytes_to_lengths[key].insert(token_bytes.len() as u16);
             }
 
             bytes_to_token_index.insert(token_bytes.clone(), token_index);
             token_index_to_bytes[token_index as usize] = token_bytes;
         }
 
+        let first_bytes_to_lengths: Vec<Box<[_]>> = first_bytes_to_lengths.into_iter()
+            .map(|inner| {
+                let mut inner: Vec<_> = inner.into_iter().collect();
+                inner.sort_unstable_by_key(|l| !*l);
+                inner.into_boxed_slice()
+            }).collect();
+
         Tokenizer {
-            first_bytes_to_len,
+            first_bytes_to_lengths,
             bytes_to_token_index,
             token_index_to_bytes,
         }
@@ -44,21 +62,25 @@ pub fn encode(input: &str) -> Vec<u16> {
     let tokenizer = &*TOKENIZER;
     let mut output = Vec::new();
     'next_token: while !input.is_empty() {
-        let mut length = input.len();
-        if length >= 2 {
-            length = std::cmp::min(
-                length,
-                tokenizer.first_bytes_to_len[u16::from_ne_bytes([input[0], input[1]]) as usize]
-            );
+        let lengths;
+        if input.len() >= 2 {
+            let key = u16::from_ne_bytes([input[0], input[1]]) as usize;
+            lengths = &tokenizer.first_bytes_to_lengths[key][..];
+        } else {
+            lengths = &[1][..];
         }
-        while length > 0 {
+
+        for &length in lengths {
+            let length = length as usize;
+            if length > input.len() {
+                continue;
+            }
+
             if let Some(&token_index) = tokenizer.bytes_to_token_index.get(&input[..length]) {
                 output.push(token_index);
                 input = &input[length..];
                 continue 'next_token;
             }
-
-            length -= 1;
         }
 
         unreachable!("no matching token found");
