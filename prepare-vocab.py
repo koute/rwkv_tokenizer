1871910d3386c512d43de4086c171112935bf599
Jan Bujak <j@exia.io>
2023-05-01 13:00:20 +0000

    Make `prepare-vocab.py` convert the vocabulary to JSON

--- prepare-vocab.py
+++ prepare-vocab.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python
 
-from struct import pack
+import json
 
 I_TO_TOKEN = {}
-sorted = [] # must be already sorted
 lines = open("rwkv_vocab_v20230424.txt", "r", encoding="utf-8").readlines()
 for l in lines:
     idx = int(l[:l.index(' ')])
     x = eval(l[l.index(' '):l.rindex(' ')])
-    x = x.encode("utf-8") if isinstance(x, str) else x
-    assert isinstance(x, bytes)
-    assert len(x) == int(l[l.rindex(' '):])
-    sorted += [x]
+    if not isinstance(x, str):
+        x = list(x)
     I_TO_TOKEN[idx] = x
 
-out = open("rwkv_vocab.bin", "wb")
-out.write(pack("<I", len(I_TO_TOKEN)))
-for token_index, token_string in I_TO_TOKEN.items():
-    out.write(pack("<I", len(token_string)))
-    out.write(token_string)
-    out.write(pack("<H", token_index))
+out = open("rwkv_vocab_v20230424.json", "w")
+out.write(json.dumps(I_TO_TOKEN, indent=4))
